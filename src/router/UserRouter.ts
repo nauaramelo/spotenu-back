0147a76e6973934df1ab6fbdbb90770859b1db74
Nauara Melo <nauaramelo@gmail.com>
2020-06-26 02:39:51 -0300

    Inclusao da rota de cadastro de administrador

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -4,4 +4,5 @@ import { UserController } from "../controller/UserController";
 export const userRouter = express.Router();
 
 userRouter.post("/signup-listener", new UserController().signupListener);
+userRouter.post("/signup-admin", new UserController().signupAdmin);
 userRouter.post("/login", new UserController().login);
