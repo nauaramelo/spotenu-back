3d75f8626f8697d68de3cc2b4a1c91a3d78f2b00
Nauara Melo <nauaramelo@gmail.com>
2020-06-26 03:26:47 -0300

    Criacao do database do genero

---
+++ src/data/GenreDatabase.ts
@@ -0,0 +1,34 @@
+import { BaseDataBase } from "./BaseDatabase";
+import { Genre } from "../model/Genre";
+
+export class GenreDatabase extends BaseDataBase {
+    protected tableName: string = "SpotenuGenres";
+
+    protected toModel(dbModel?: any): Genre | undefined {
+        return (
+            dbModel && 
+            new Genre(
+                dbModel.id, 
+                dbModel.name
+            )
+        )
+    } 
+
+    public async createGenre(genre: Genre): Promise<void> {
+        await super.getConnection().raw(`
+          INSERT INTO ${this.tableName} (id, name)
+            VALUES (
+                '${genre.getId()}',
+                '${genre.getName()}'
+            )
+        `)
+    } 
+
+    public async findByName(name: string): Promise<Genre | undefined> {
+        const result = await super.getConnection().raw(`
+            SELECT * FROM ${this.tableName} where name = '${name}'
+        `)
+
+        return this.toModel(result[0][0])
+    }
+}
