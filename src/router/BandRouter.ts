3a6076f21af7d52b0f59ace189adf4be6d4680ad
Nauara Melo <nauaramelo@gmail.com>
2020-06-26 03:16:20 -0300

    inclusao da rota de aprovar bandas

--- src/router/BandRouter.ts
+++ src/router/BandRouter.ts
@@ -4,5 +4,6 @@ import { BandController } from "../controller/BandController";
 
 export const bandRouter = express.Router();
 
+bandRouter.post("/:id/approve", new BandController().aprroveBand);
 bandRouter.post("/signup", new BandController().signupUserBand);
 bandRouter.get("/all", new BandController().getAllBands);
